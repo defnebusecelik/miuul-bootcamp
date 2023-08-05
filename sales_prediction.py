a3cde41cd74e9061b415f7630875c31e77ca6ca7
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2023-08-06 00:13:33 +0300

    Update sales_prediction.py

--- sales_prediction.py
+++ sales_prediction.py
@@ -37,10 +37,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.impute import KNNImputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LinearRegression, Ridge, RidgeCV, ElasticNet, Lasso, LassoCV
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, accuracy_score, recall_score, precision_score, f1_score, roc_auc_score
 from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
 from sklearn.neighbors import LocalOutlierFactor
@@ -559,3 +560,112 @@ ax.set_title("Correlation Matrix", fontsize=20)
 plt.show()
 
 #Data PreProcessing
+#Feature Engineering
+
+df['NEW_Avg_AtBat'] = df['CAtBat'] / df['Years']
+df['NEW_Avg_Hits'] = df['CHits'] / df['Years']
+df['NEW_Avg_HmRun'] = df['CHmRun'] / df['Years']
+df['NEW_Avg_Runs'] = df['CRuns'] / df['Years']
+df['NEW_Avg_RBI'] = df['CRBI'] / df['Years']
+df['NEW_Avg_Walks'] = df['CWalks'] / df['Years']
+print(df.head())
+'''
+   AtBat  Hits  HmRun  Runs  RBI  Walks  Years  CAtBat   CHits  CHmRun  CRuns  CRBI  CWalks League Division  PutOuts  Assists  Errors  Salary NewLeague  NEW_Avg_AtBat  \
+0    293    66      1    30   29     14      1     293   66.00    1.00     30    29   14.00      A        E      446       33      20     NaN         A         293.00   
+1    315    81      7    24   38     39     14    3449  835.00   69.00    321   414  375.00      N        W      632       43      10  475.00         N         246.36   
+2    479   130     18    66   72     76      3    1624  457.00   63.00    224   266  263.00      A        W      880       82      14  480.00         A         541.33   
+3    496   141     20    65   78     37     11    5628 1575.00  225.00    828   838  354.00      N        E      200       11       3  500.00         N         511.64   
+4    321    87     10    39   42     30      2     396  101.00   12.00     48    46   33.00      N        E      805       40       4   91.50         N         198.00   
+
+   NEW_Avg_Hits  NEW_Avg_HmRun  NEW_Avg_Runs  NEW_Avg_RBI  NEW_Avg_Walks  
+0         66.00           1.00         30.00        29.00          14.00  
+1         59.64           4.93         22.93        29.57          26.79  
+2        152.33          21.00         74.67        88.67          87.67  
+3        143.18          20.45         75.27        76.18          32.18  
+4         50.50           6.00         24.00        23.00          16.50  
+'''
+
+def label_encoder(dataframe, binary_col):
+    labelencoder = LabelEncoder()
+    dataframe[binary_col] = labelencoder.fit_transform(dataframe[binary_col])
+    return dataframe
+binary_cols = [col for col in df.columns if df[col].dtypes == "O" and df[col].nunique() == 2]
+for col in binary_cols:
+    df = label_encoder(df, col)
+print(df.head())
+'''
+   AtBat  Hits  HmRun  Runs  RBI  Walks  Years  CAtBat   CHits  CHmRun  CRuns  CRBI  CWalks  League  Division  PutOuts  Assists  Errors  Salary  NewLeague  \
+0    293    66      1    30   29     14      1     293   66.00    1.00     30    29   14.00       0         0      446       33      20     NaN          0   
+1    315    81      7    24   38     39     14    3449  835.00   69.00    321   414  375.00       1         1      632       43      10  475.00          1   
+2    479   130     18    66   72     76      3    1624  457.00   63.00    224   266  263.00       0         1      880       82      14  480.00          0   
+3    496   141     20    65   78     37     11    5628 1575.00  225.00    828   838  354.00       1         0      200       11       3  500.00          1   
+4    321    87     10    39   42     30      2     396  101.00   12.00     48    46   33.00       1         0      805       40       4   91.50          1   
+
+   NEW_Avg_AtBat  NEW_Avg_Hits  NEW_Avg_HmRun  NEW_Avg_Runs  NEW_Avg_RBI  NEW_Avg_Walks  
+0         293.00         66.00           1.00         30.00        29.00          14.00  
+1         246.36         59.64           4.93         22.93        29.57          26.79  
+2         541.33        152.33          21.00         74.67        88.67          87.67  
+3         511.64        143.18          20.45         75.27        76.18          32.18  
+4         198.00         50.50           6.00         24.00        23.00          16.50  
+'''
+
+print(binary_cols)
+'''
+['League', 'Division', 'NewLeague']
+'''
+cat_cols = [col for col in cat_cols if col not in binary_cols]
+def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+df = one_hot_encoder(df, cat_cols, drop_first=True)
+print(df.head())
+'''
+   AtBat  Hits  HmRun  Runs  RBI  Walks  Years  CAtBat   CHits  CHmRun  CRuns  CRBI  CWalks  League  Division  PutOuts  Assists  Errors  Salary  NewLeague  \
+0    293    66      1    30   29     14      1     293   66.00    1.00     30    29   14.00       0         0      446       33      20     NaN          0   
+1    315    81      7    24   38     39     14    3449  835.00   69.00    321   414  375.00       1         1      632       43      10  475.00          1   
+2    479   130     18    66   72     76      3    1624  457.00   63.00    224   266  263.00       0         1      880       82      14  480.00          0   
+3    496   141     20    65   78     37     11    5628 1575.00  225.00    828   838  354.00       1         0      200       11       3  500.00          1   
+4    321    87     10    39   42     30      2     396  101.00   12.00     48    46   33.00       1         0      805       40       4   91.50          1   
+
+   NEW_Avg_AtBat  NEW_Avg_Hits  NEW_Avg_HmRun  NEW_Avg_Runs  NEW_Avg_RBI  NEW_Avg_Walks  
+0         293.00         66.00           1.00         30.00        29.00          14.00  
+1         246.36         59.64           4.93         22.93        29.57          26.79  
+2         541.33        152.33          21.00         74.67        88.67          87.67  
+3         511.64        143.18          20.45         75.27        76.18          32.18  
+4         198.00         50.50           6.00         24.00        23.00          16.50  
+'''
+
+df_null = df[df["Salary"].isnull()]
+df.dropna(inplace=True)
+print(df.isnull().values.any())       #False
+
+#Standard Scaling
+
+num_cols.remove("Salary")
+scaler = StandardScaler()
+df[num_cols] = scaler.fit_transform(df[num_cols])
+print(df.head())
+'''
+   AtBat  Hits  HmRun  Runs   RBI  Walks  Years  CAtBat  CHits  CHmRun  CRuns  CRBI  CWalks  League  Division  PutOuts  Assists  Errors  Salary  NewLeague  \
+1  -0.60 -0.60  -0.53 -1.21 -0.52  -0.10   1.40    0.35   0.18    0.00  -0.12  0.26    0.44       1         1     1.22    -0.52    0.21  475.00          1   
+2   0.51  0.49   0.73  0.44  0.79   1.61  -0.90   -0.45  -0.41   -0.07  -0.42 -0.20    0.01       0         1     2.11    -0.25    0.82  480.00          0   
+3   0.63  0.74   0.96  0.40  1.03  -0.19   0.77    1.30   1.33    1.93   1.41  1.57    0.36       1         0    -0.32    -0.74   -0.85  500.00          1   
+4  -0.56 -0.46  -0.19 -0.62 -0.37  -0.51  -1.11   -0.99  -0.97   -0.71  -0.95 -0.88   -0.86       1         0     1.84    -0.54   -0.70   91.50          1   
+5   1.29  1.36  -0.87  0.76 -0.02  -0.28   0.77    0.77   0.64   -0.62   0.42  0.02   -0.25       0         1    -0.03     2.09    2.49  750.00          0   
+'''
+
+#Modelling
+y = df['Salary']
+X = df.drop("Salary", axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=13)
+
+linreg = LinearRegression()
+linreg_model = linreg.fit(X_train,y_train)
+y_pred = linreg_model.predict(X_test)
+linreg_rmse = np.sqrt(mean_squared_error(y_test,y_pred))
+print(linreg_rmse)
+'''
+385.2631012909874
+'''
+print(linreg_model.score(X_train,y_train))
+#0.6208492943425906
